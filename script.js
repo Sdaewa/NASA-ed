f73a71b1d6fda005612b2e1e0545ab2ef716564a
Luis Ramirez <sdaewa22@gmail.com>
2020-11-11 11:24:54 +0000

    dinamic population of DOM

--- script.js
+++ script.js
@@ -1,17 +1,75 @@
+const resultNav = document.getElementById('resultNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
 const count = 10;
 const APIKey = '2yoYXehDcL6EPw9MsFGLNPO1hZdDvVtHYBAzM69D';
-const APIUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${APIKey}&count=${count}`;
+const APIUrl = `https://api.nasa.gov/planetary/apod?api_key=${APIKey}&count=${count}`;
+
 
 let resultsArray = [];
+
+
+function updateDOM() {
+    resultsArray.forEach((result) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const link = document.createElement('a');
+        link.href = result.hdurl;
+        link.title = 'View Full Image';
+        link.targe = '_blank';
+
+        const image = document.createElement('img');
+        image.src = result.url;
+        image.alt = 'NASA pic';
+        image.loading = 'lazy';
+        image.classList.add('card-img-top');
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+        const cardTitle = document.createElement('card-title');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = result.title;
+
+        const saveText = document.createElement('p');
+        saveText.classList.add('clickable');
+        saveText.textContent = 'Add to favorites';
+
+        const cardText = document.createElement('p');
+        card.textContent = result.explanation;
+
+        const footer = document.createElement('small');
+        footer.classList.add('text-muted');
+
+        const date = document.createElement('strong');
+        date.textContent = result.date;
+
+        const copyright = document.createElement('span');
+        copyright.textContent = `${result.copyright}`;
+
+        footer.append(date, copyright);
+        cardBody.append(cardTitle, saveText, cardText, footer);
+        link.appendChild(image);
+        card.append(link, cardBody);
+        imagesContainer.appendChild(card);
+    });
+}
+
+
+
 
 async function getNasaImg() {
     try {
         const reponse = await fetch(APIUrl);
         resultsArray = await reponse.json();
         console.log(resultsArray);
-
+        updateDOM();
     } catch (error) {
-
+        console.log(error);
     }
 }
 
